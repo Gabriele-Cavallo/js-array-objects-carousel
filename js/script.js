2bf90145b109cfc99823102d9d73cbd67d460f23
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-21 15:51:31 +0100

    feat: add bonus tasks

--- js/script.js
+++ js/script.js
@@ -14,6 +14,10 @@
 // la freccia verso l'alto, la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se 
 // l'utente clicca la freccia verso il basso.
 
+// BONUS 1: Aggiungere le thumbnails (sottoforma di miniatura) ed al click attivare l’immagine corrispondente.
+// BONUS 2: Aggiungere funzionalità di autoplay: dopo un certo periodo di tempo (3 secondi) l’immagine attiva dovrà cambiare alla successiva.
+// BONUS 3: Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay.
+
 
 const images = [
     {
